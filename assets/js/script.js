bd4dd644484f1939dc8e5ef8d83b9db93a7edbcd
Joon Kim <joonkidk@gmail.com>
2022-11-18 15:38:38 -0500

    created navbar, added bulma, added logo to top left nav, created menu icons and dropdown menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,17 @@
-// const now = setInterval(getApi, 10000);
+// const update = setInterval(getBtcApi, 1000);
 
-// const baseURL = "https://rest.coinapi.io/v1/assets";
-const btcApi = 'https://rest.coinapi.io/v1/exchanges?filter_exchange_id=btc'
-const options = {
-  headers: {
-    Authorization: "41254972-9103-42DA-B4AC-F785AF290950"
-  }
-};
-
-function getApi() {
-  fetch(btcApi, options)
-  .then( res => res.json() )
-  .then( data => {
-    console.log(data);
-    // btcData(data) 
-  });
+function getBtcApi() {
+  let requestURL = `https://api.coincap.io/v2/assets/bitcoin`
+  fetch(requestURL)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data)
+    updateBtc(data)
+  })
 }
 
-// let btcData = function (data) {
-//   let btcLivePrice = document.querySelector('#btcLivePrice')
-//   btcLivePrice.innerText = data[1].price_usd;
-// }
-
-// function getBtcApi() {
-//   let btcApi = 'https://rest.coinapi.io/v1/exchanges?filter_exchange_id=btc'
-//   fetch(btcApi, options)
-//   .then( res => res.json() )
-//   .then((data) => {
-//     console.log(data);
-//     btcTracker(data);
-//   });
-// }
-// //   // let btcTracker = function(coinData) {
-
-// //   // }
-// getBtcApi()
+let updateBtc = function(btcData) {
+  document.querySelector('#btcLivePrice').innerText = "$" + btcData.data.priceUsd.slice(0, 8);
+}
